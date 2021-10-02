19b036cff3c5bf6270afc8f2e97d417a1b3e59fb
Nehal Gala <nehalgala@Apples-MacBook-Pro.local>
2021-10-02 19:24:16 +0530

    changes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require('../controllers/auth.controller');
 
-// Create routes for user here
+// Create routes for auth here
 router.post('/login', AuthController.login);
 module.exports = router;
